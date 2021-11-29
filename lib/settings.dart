b48df235cf6edc7209236d3adc36a4c71d77704e
Xunafay <me@xunafay.io>
2021-11-29 21:11:40 +0100

    feat: spotify sdk integration

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -17,25 +16,8 @@ class _SettingsPageState extends State<SettingsPage> {
       ),
       body: Center(
         child: ElevatedButton(
-          onPressed: () async {
-            GoogleSignIn _googleSignIn = GoogleSignIn(
-              scopes: [
-                'email',
-                'profile',
-                'https://www.googleapis.com/auth/calendar.events.readonly',
-                // 'https://www.googleapis.com/auth/gmail.readonly', // notify of events/purchases
-                'https://www.googleapis.com/auth/contacts.readonly',
-                'https://www.googleapis.com/auth/user.birthday.read',
-              ],
-            );
-            try {
-              var user = await _googleSignIn.signIn();
-              debugPrint(user?.displayName);
-            } catch (error) {
-              print(error);
-            }
-          },
-          child: const Text('Google Sign In'),
+          onPressed: () async {},
+          child: const Text('Spotify Sign In'),
         ),
       ),
     );
